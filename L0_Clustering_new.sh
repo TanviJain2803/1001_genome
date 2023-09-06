2fce855d64ea39dab418e91a6c5b4a638fedf092
TanviJain2803 <73011639+TanviJain2803@users.noreply.github.com>
2023-09-06 14:48:12 -0700

    Create L0_Clustering_new.sh

---
+++ L0_Clustering_new.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N tanvi_test
+#$ -cwd
+#$ -pe smp 12
+#$ -l h_vmem=12G
+#$ -V
+#$ -e error_log_file
+#$ -o output_log_file
+#$ -q gale.q
+
+cd /gale/ddn/ddn_neomorph/tjain
+
+Rscript L0_Clustering.R "$1"
