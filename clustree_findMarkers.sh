53bfa2901067efd1a66e343e1e9e3ddc13ed227b
TanviJain2803 <73011639+TanviJain2803@users.noreply.github.com>
2023-09-25 11:14:11 -0700

    Update clustree_findMarkers.sh

--- clustree_findMarkers.sh
+++ clustree_findMarkers.sh
@@ -4,8 +4,8 @@
 #$ -pe smp 4
 #$ -l h_vmem=12G
 #$ -V
-#$ -e error_log_file
-#$ -o output_log_file
+#$ -e error_log_file_clus
+#$ -o output_log_file_clus
 #$ -q gale.q
 
 cd /gale/ddn/ddn_neomorph/tjain/1001_genome
